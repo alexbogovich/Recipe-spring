8c51b491100bb713b050998c470ce3768327c714
chillten <bogovich.alexander@gmail.com>
2017-08-06 21:41:58 +0700

    Add find by findByDescription in repositories

--- src/main/java/com/bogovich/recipe/controllers/IndexController.java
+++ src/main/java/com/bogovich/recipe/controllers/IndexController.java
@@ -1,13 +1,36 @@
 package com.bogovich.recipe.controllers;
 
+import com.bogovich.recipe.models.Category;
+import com.bogovich.recipe.models.UnitOfMeasure;
+import com.bogovich.recipe.repositories.CategoryRepository;
+import com.bogovich.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("Cat desc is: " + categoryOptional.get().getDescription());
+
+        System.out.println("Uom id is: " + unitOfMeasureOptional.get().getId());
+        System.out.println("Uom desc is: " + unitOfMeasureOptional.get().getDescription());
         return "index";
     }
 }
--- src/main/java/com/bogovich/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/bogovich/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.bogovich.recipe.repositories;
 import com.bogovich.recipe.models.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository <UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
