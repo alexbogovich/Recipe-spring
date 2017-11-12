8f92d152bf9cca85fe18a7430c08430e533ca479
chillten <bogovich.alexander@gmail.com>
2017-11-12 23:56:27 +0700

    Add ingredient validation

--- src/main/java/com/bogovich/recipe/controllers/IngredientController.java
+++ src/main/java/com/bogovich/recipe/controllers/IngredientController.java
@@ -8,15 +8,19 @@ import com.bogovich.recipe.services.UnitOfMeasureService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Slf4j
 @Controller
 public class IngredientController {
 
+    public static final String RECIPE_INGREDIENTFORM_URL = "recipe/ingredient/ingredientform";
     private final RecipeService recipeService;
     private final IngredientService ingredientService;
     private final UnitOfMeasureService unitOfMeasureService;
@@ -53,7 +57,7 @@ public class IngredientController {
                            ingredientService.findByRecipeIdAndIngridientId(recipeId, ingredientId).block());
         model.addAttribute("uomList", unitOfMeasureService.listAllUoms().collectList().block());
         model.addAttribute("recipeId", recipeId);
-        return "recipe/ingredient/ingredientform";
+        return RECIPE_INGREDIENTFORM_URL;
     }
 
     @GetMapping("recipe/{recipeId}/ingredient/new")
@@ -61,15 +65,21 @@ public class IngredientController {
         model.addAttribute("ingredient", new Ingredient());
         model.addAttribute("uomList", unitOfMeasureService.listAllUoms().collectList().block());
         model.addAttribute("recipeId", recipeId);
-        return "recipe/ingredient/ingredientform";
+        return RECIPE_INGREDIENTFORM_URL;
     }
 
     @PostMapping("recipe/{recipeId}/ingredient")
     public String saveOrUpdate(@PathVariable String recipeId,
-                               @ModelAttribute Ingredient ingredient) {
+                               @Valid @ModelAttribute Ingredient ingredient, BindingResult bindingResult, Model model) {
         log.info(ingredient.toString());
-        ingredientService.saveIngredient(recipeId, ingredient).block();
-        return String.format("redirect:/recipe/%s/ingredients", recipeId);
+        if(!bindingResult.hasErrors()) {
+            ingredientService.saveIngredient(recipeId, ingredient).block();
+            return String.format("redirect:/recipe/%s/ingredients", recipeId);
+        } else {
+            log.debug(bindingResult.getAllErrors().toString());
+            model.addAttribute("uomList", unitOfMeasureService.listAllUoms().collectList().block());
+            return RECIPE_INGREDIENTFORM_URL;
+        }
     }
 
     @PostMapping("recipe/{recipeId}/ingredient/{ingredientId}/delete")
